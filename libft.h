7f40db0c58c59ffe42ff54c1e55ae595d0687b2c
Aarni Torma <atorma@student.hive.fi>
2024-04-16 17:52:55 +0300

    memchr def

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 17:37:08 by atorma            #+#    #+#             */
-/*   Updated: 2024/04/16 17:49:50 by atorma           ###   ########.fr       */
+/*   Updated: 2024/04/16 17:52:31 by atorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
